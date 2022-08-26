5c8b1bef4752c745f810ea0b9a1fcfc1dc42470f
T-rex <0x50Fc@gmail.com>
2022-08-26 14:51:09 +0800

    ci

--- .github/ci/publish.sh
+++ .github/ci/publish.sh
@@ -6,12 +6,8 @@ go install github.com/ability-sh/ability@v1.0.0
 
 echo $GOPATH
 
-ABILITY=/root/go/bin/ability
-
-chmod +x $ABILITY
-
-ENV_OS=`$ABILITY env os`
-ENV_ARCH=`$ABILITY env arch`
+ENV_OS=`ability env os`
+ENV_ARCH=`ability env arch`
 
 mkdir dist
 mkdir dist/cloud
@@ -19,4 +15,5 @@ mkdir dist/cloud/bin
 
 mv abi-app-store dist/cloud/bin/$ENV_OS-$ENV_ARCH
 
-$ABILITY app publish 
+ability app publish -token $ABI_TOKEN -file ./app.yaml
+
