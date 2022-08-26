c74a787d00dee295ab2a15478b22129e2018960a
T-rex <0x50Fc@gmail.com>
2022-08-26 15:07:08 +0800

    add

--- .github/ci/publish.sh
+++ .github/ci/publish.sh
@@ -4,10 +4,10 @@ PWD_DIR=$(pwd)
 
 go install github.com/ability-sh/ability@v1.0.0
 
-echo $GOPATH
+ABILITY=./.github/ci/ability
 
-ENV_OS=`ability env os`
-ENV_ARCH=`ability env arch`
+ENV_OS=`$ABILITY env os`
+ENV_ARCH=`$ABILITY env arch`
 
 mkdir dist
 mkdir dist/cloud
@@ -15,5 +15,5 @@ mkdir dist/cloud/bin
 
 mv abi-app-store dist/cloud/bin/$ENV_OS-$ENV_ARCH
 
-ability app publish -token $ABI_TOKEN -file ./app.yaml
+$ABILITY app publish -token $ABI_TOKEN -file ./app.yaml
 
